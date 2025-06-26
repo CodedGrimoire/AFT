8a4e5cbaf8936c56dbe627b5e65508ec64e7d3b9
Tazkia Malik <146949735+CodedGrimoire@users.noreply.github.com>
2025-06-26 20:23:19 +0600

    asc

---
+++ demo/src/main/java/com/networking/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.networking.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
